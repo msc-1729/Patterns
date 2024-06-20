28929f4255bed1280b7c2928cf078b2a219e66d5
msc-1729 <sarathmahamkali@gmail.com>
2024-06-20 19:38:56 -0400

    SARLC-0620 optimized implemetation

--- N2Forest.java
+++ N2Forest.java
@@ -2,8 +2,13 @@ import java.util.Scanner;
 public class N2Forest {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        System.out.println("You have two choices for printing the N2 forest:");
-        System.out.println("1. Characters , 2. Numericals , 3.repeatForest, 4.n2ReverseForest, 5.n2ReverseNumerical, 6.n2ReverseNumericalReverseOrder");
+        System.out.println("You have these choices for printing the N2 forest:");
+        System.out.println("1. Characters");
+        System.out.println("2. Numericals");
+        System.out.println("3. Repeat Forest");
+        System.out.println("4. N2 Reverse Forest");
+        System.out.println("5. N2 Reverse Numericals");
+        System.out.println("6. N2 Reverse Numericals Reverse Order");
         System.out.println("Enter your choice");
         Integer choice = scan.nextInt();
         System.out.println("Enter the size of the forest");
@@ -84,3 +89,74 @@ public class N2Forest {
         }
     }
 }
+
+
+// optimized way of implementing this
+
+// import java.util.Scanner;
+
+// public class N2Forest {
+//     public static void main(String[] args){
+//         Scanner scan = new Scanner(System.in);
+//         System.out.println("You have these choices for printing the N2 forest:");
+//         System.out.println("1. Characters");
+//         System.out.println("2. Numericals");
+//         System.out.println("3. Repeat Forest");
+//         System.out.println("4. N2 Reverse Forest");
+//         System.out.println("5. N2 Reverse Numericals");
+//         System.out.println("6. N2 Reverse Numericals Reverse Order");
+//         System.out.println("Enter your choice:");
+//         int choice = scan.nextInt();
+//         System.out.println("Enter the size of the forest:");
+//         int size = scan.nextInt();
+//         scan.close();
+        
+//         switch (choice) {
+//             case 1:
+//                 printForest(size, "*", false, false);
+//                 break;
+//             case 2:
+//                 printForest(size, "numerical", false, false);
+//                 break;
+//             case 3:
+//                 printForest(size, "repeat", false, false);
+//                 break;
+//             case 4:
+//                 printForest(size, "*", true, false);
+//                 break;
+//             case 5:
+//                 printForest(size, "numerical", true, false);
+//                 break;
+//             case 6:
+//                 printForest(size, "reverseNumerical", true, true);
+//                 break;
+//             default:
+//                 System.out.println("Invalid choice.");
+//                 break;
+//         }
+//     }
+
+//     public static void printForest(int size, String type, boolean reverse, boolean reverseOrder) {
+//         for (int i = (reverse ? size : 1); (reverse ? i > 0 : i <= size); i = (reverse ? i - 1 : i + 1)) {
+//             StringBuilder sb = new StringBuilder();
+//             for (int j = 1; j <= i; j++) {
+//                 switch (type) {
+//                     case "*":
+//                         sb.append("*");
+//                         break;
+//                     case "numerical":
+//                         sb.append(j);
+//                         break;
+//                     case "repeat":
+//                         sb.append(i);
+//                         break;
+//                     case "reverseNumerical":
+//                         sb.append(reverseOrder ? i - j + 1 : j);
+//                         break;
+//                 }
+//             }
+//             System.out.println(sb.toString());
+//         }
+//     }
+// }
+
